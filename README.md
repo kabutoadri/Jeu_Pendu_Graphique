dd58e5b91567cc3134004bbd9bbe18d10108e05d
Adri <adriano.alvesmorais.095@gmail.com>
2025-12-26 15:53:59 +0100

    Ajout des indices et améliorations de l'interface général du jeu.

---
+++ README.md
@@ -0,0 +1,24 @@
+Jeux du Pendu en interface graphique
+=============
+
+### Identification du groupe et du travail de chacun
+    Le groupe de travail est composé d'Adriano Alves Morais et Marc Schilter.
+
+    Marc Schilter se charge de gérer l'affichage du dessin du pendu ainsi que l'interface du résultat (Perdu ou gagné).
+
+    Adriano Alves Morais se charge de l'interface d'insertion des lettres avec gestion des erreurs et de la gestion des
+    indices pour l'utilisateur. Il s'occupe également de l'apparence général du jeu. Il s'occupe aussi de la rédaction
+    du fichier readme.md.
+
+### Formalisation des objectifs
+    nous avons pour objectif de créer un jeu du pendu avec tkinter fonctionnel. Nous devons utiliser obligatoirement
+    pack, place et grid.
+
+    Le but du jeu est de faire deviner le mot mystère au joueur. Pour ce faire, il a 7 tentatives et il peu proposer une
+    lettre à chaque tentative. À la fin de la partie le joueur peut soit quitter le jeu soit rejouer. Pour lancer le
+    programme, il est nécéssaire d'installer le package Pillow.
+
+### Sources identifiées
+    github.com
+    w3schools.com
+    chatgpt.com
--- main.py
+++ main.py
@@ -9,9 +9,22 @@ from PIL import Image, ImageTk  # Pillow
 #######################################################################################################################
 # Fonction mot aléatoire
 def mots_aleatoires(index, lettre, proposition_vide):
-    listes_mots = ["python", "ordinateur", "jeu", "random", "chat", "chien", "soleil", "lune", "code", "apprentissage"]
+    #listes_mots = ["python", "ordinateur", "jeu", "random", "chat", "chien", "soleil", "lune", "code", "apprentissage"]
+    dict_mots = {"python": "Langage utilisé pour créer le jeu",
+                 "ordinateur": "Utilisé pour lancer le jeu",
+                 "jeu":"Qu'est-ce qu'un Pendu?",
+                 "random":"1, 4, 2, 5, 3, ...",
+                 "chat": "Animal",
+                 "chien": "Animal",
+                 "soleil": "Jour",
+                 "lune": "nuit",
+                 "code": "Programmation",
+                 "apprentissage": "CPNV"
+                 }
+    listes_index = list(dict_mots.keys())
 
-    mot_mystere = list(listes_mots[index].upper())
+    mot_mystere = list(listes_index[index].upper())
+    #mot_mystere = list(listes_mots[index].upper())
 
     if proposition_vide == "":
         affichage_mot = ["_"] * len(mot_mystere)
@@ -22,7 +35,7 @@ def mots_aleatoires(index, lettre, proposition_vide):
         if char == lettre:
             affichage_mot[i] = lettre
 
-    return mot_mystere, affichage_mot
+    return mot_mystere, affichage_mot, dict_mots[listes_index[index]]
 
 #######################################################################################################################
 # Fonction image pendu (JPG)
@@ -52,15 +65,30 @@ lettre_choisi = ""
 numero_random = random.randint(0, 9)
 stockage = mots_aleatoires(numero_random, "", "")[1]
 mot_visible = mots_aleatoires(numero_random, lettre_choisi, stockage)[0]  # mot aléatoire (liste de lettres)
+indice = mots_aleatoires(numero_random, lettre_choisi, stockage)[2]
+lettres_utilisees = [] # liste des lettres déjà proposées
+message_indice = "Passe ta souris ici pour l'indice"
 
-window_width = 500
-window_height = 650
-Bg_Color= "#a6a7ab"
+#######################################################################################################################
+# Paramètre de la fenêtre
+window_width = 550
+window_height = 750
+bg_color= "#f0f8ff"
+text_color = "#333333"
+titre_color = "#ff69b4"
+cacher_color = "#00bfff"
+highlight_color = "#ff4d4d"
+utiliser_color = "#ff4500"
+font_titre = ("Comic Sans MS", 20, "bold")
+font_text = ("Comic Sans MS", 14)
+font_entry = ("Comic Sans MS", 16, "bold")
+font_cacher = ("Courier New", 24, "bold")
+
 #######################################################################################################################
 # Tkinter
 root = tk.Tk()
 root.title("Pendu")
-root.configure(bg=Bg_Color)
+root.configure(bg=bg_color)
 
 # Dimensions de l'écran
 screen_width = root.winfo_screenwidth()
@@ -72,27 +100,38 @@ position_y = int((screen_height / 2) - (window_height / 2))
 root.geometry(f"{window_width}x{window_height}+{position_x}+{position_y}")
 
 #######################################################################################################################
+# Titre
+label_titre = tk.Label(root, text="Jeu du Pendu", bg=bg_color, font=font_titre, fg=titre_color)
+label_titre.pack(side="top", pady=10)
+
 # Zone saisie utilisateur
-frame_User_Entry = tk.Frame(root, bg=Bg_Color)
+frame_User_Entry = tk.Frame(root, bg=bg_color)
 frame_User_Entry.pack(side="top", pady=10)
 
-label_Instruction = tk.Label(frame_User_Entry, text="Choisis une lettre :", bg=Bg_Color)
-label_Instruction.pack(side="top")
+label_Instruction = tk.Label(frame_User_Entry, text="Choisis une lettre :", bg=bg_color, font=font_text, fg=text_color)
 label_Instruction.grid(row=0, column=0, padx=5)
 
-entry_Lettre = tk.Entry(frame_User_Entry, width=10)
+entry_Lettre = tk.Entry(frame_User_Entry, width=10, font=font_entry, fg=text_color)
 entry_Lettre.grid(row=0, column=1, padx=5)
 entry_Lettre.focus()
 
-label_Lettres = tk.Label(root, text=" ".join(stockage), font=("Arial", 22), bg=Bg_Color)
+label_Lettres = tk.Label(root, text=" ".join(stockage), font=font_cacher, bg=bg_color, fg=cacher_color)
 label_Lettres.pack(side="top", pady=10)
+
+# Lettres déjà utiisées
+label_Utiliser = tk.Label(root, text="Lettres déjà utilisées : ", bg=bg_color, font=font_text, fg=utiliser_color)
+label_Utiliser.pack(side="top", pady=50)
+
+# indices
+label_Indice = tk.Label(root, text=message_indice, width=len(message_indice), bg=bg_color, font=font_text, fg=text_color)
+label_Indice.pack(side="bottom", pady=40)
 
 #######################################################################################################################
 # Frame du pendu (image) placé EN BAS avec place
-frame_Pendu = tk.Frame(root, width=280, height=280, bd=2, relief="groove", bg=Bg_Color)
+frame_Pendu = tk.Frame(root, width=280, height=280, bd=2, relief="groove", bg=bg_color)
 frame_Pendu.place(relx=0.5, rely=1.0, anchor="s", y=-130)
 
-label_Pendu = tk.Label(frame_Pendu, bg=Bg_Color)
+label_Pendu = tk.Label(frame_Pendu, bg=bg_color, fg=text_color)
 label_Pendu.place(relx=0.5, rely=0.5, anchor="center")
 
 photo_pendu = None  # référence (IMPORTANT)
@@ -116,10 +155,12 @@ update_pendu()
 # Reset complet de la partie
 def reset_game():
     """Réinitialise la partie: erreur=0, nouveau mot, stockage reset, labels + image."""
-    global erreur, lettre_choisi, numero_random, stockage, mot_visible
+    global erreur, lettre_choisi, numero_random, stockage, mot_visible, lettres_utilisees
 
     erreur = 0
     lettre_choisi = ""
+    lettres_utilisees = []
+    label_Utiliser.config(text="Lettres déjà utilisées : ")
 
     # Choisir un autre mot (différent si possible)
     ancien_index = numero_random
@@ -144,48 +185,52 @@ def show_end_popup(is_win: bool):
 
     top = tk.Toplevel(root)
     top.title(title)
-    top.configure(bg=Bg_Color)
+    top.configure(bg=bg_color)
     top.geometry(f"320x170+{position_x + 90}+{position_y + 120}")
     top.transient(root)
+    # Évite les interactions avec la fenêtre principale
     top.grab_set()
+    top.focus_force()
 
-    lbl = tk.Label(top, text=message, font=("Arial", 14, "bold"), bg=Bg_Color)
+    lbl = tk.Label(top, text=message, font=("Arial", 14, "bold"), bg=bg_color)
     lbl.pack(side="top", pady=20)
 
     #Afficher le mot si défaite
     if not is_win:
         mot_str = "".join(mot_visible)
-        lbl_mot = tk.Label(top, text=f"Le mot était : {mot_str}", font=("Arial", 12), bg=Bg_Color)
+        lbl_mot = tk.Label(top, text=f"Le mot était : {mot_str}", font=("Arial", 12), bg=bg_color)
         lbl_mot.pack(side="top", pady=5)
 
-    frame_btn = tk.Frame(top, bg=Bg_Color)
+    frame_btn = tk.Frame(top, bg=bg_color)
     frame_btn.pack(side="bottom", pady=12)
 
     def recommencer_et_fermer():
         reset_game()
         top.destroy()
 
-    btn_relance = tk.Button(frame_btn, text="Recommencer", command=recommencer_et_fermer)
+    btn_relance = tk.Button(frame_btn, text="Rejouer", command=recommencer_et_fermer, font=font_text, bg=cacher_color, fg="white", width=10)
     btn_relance.grid(row=0, column=0, padx=6)
 
-    btn_quit = tk.Button(frame_btn, text="Quitter", command=root.quit)
+    btn_quit = tk.Button(frame_btn, text="Quitter", command=root.quit, font=font_text, bg=highlight_color, fg="white", width=10)
     btn_quit.grid(row=0, column=1, padx=6)
 
 #######################################################################################################################
 # Contrôle des propositions
 def proposition(event):
-    global erreur, stockage, lettre_choisi
+    global erreur, stockage, lettre_choisi, lettres_utilisees
 
     lettre_choisi = entry_Lettre.get().strip().upper()
     entry_Lettre.delete(0, tk.END)
 
-    # Contrôle: 1 lettre alpha
+    # Contrôle : 1 lettre alpha
     if len(lettre_choisi) != 1 or not lettre_choisi.isalpha():
         return
 
-    # Mauvaise lettre => erreur + update image
-    if lettre_choisi not in mot_visible:
+    # Mauvaise lettre => erreur + update image et lettres utilisées
+    if (lettre_choisi not in mot_visible) and (lettre_choisi not in lettres_utilisees):
         erreur += 1
+        lettres_utilisees.append(lettre_choisi)
+        label_Utiliser.config(text="Lettres déjà utilisées : " + " ".join(lettres_utilisees))
         update_pendu()
 
     # Met à jour l'affichage du mot
@@ -206,6 +251,15 @@ def proposition(event):
         show_end_popup(is_win=False)
         return
 
+def survol_label(event):
+    label_Indice.config(text="indice: " + indice)
+
+def quitter_label(event):
+    label_Indice.config(text=message_indice)
+
+# bind pour la gestion des événements
+label_Indice.bind("<Enter>", survol_label)
+label_Indice.bind("<Leave>", quitter_label)
 entry_Lettre.bind("<Return>", proposition)
 
 #######################################################################################################################
